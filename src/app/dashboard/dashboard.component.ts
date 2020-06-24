332891b19c6b2b8254ca75496c84fba5d5d8b4bd
davids-pro <54986025+davids-pro@users.noreply.github.com>
2020-06-24 09:53:34 +0200

    END

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { MoviesService } from '../movies.service';
-import { Movie } from '../movie.model';
 import { Router } from '@angular/router';
 
 @Component({
